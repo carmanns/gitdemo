538efa23f6e6bcda82bda9a56930ddec234d1a94
cmd <carmanns@gmail.com>
2014-12-07 12:40:29 +0100

    Primeros cambios en el html
    - Personalizar index.php
    - Agregar carpeta css y hoja de estilos

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Awesome HTML5 Webpage</title>
+	<meta name="description" content="An awesome HTML5 page YOU built from scratch!">
+	<meta name="author" content="CMDiaz">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div id="page">
+	<div class="logo"></div>
+	<ul id="navigation">
+		<li><a href="">Item 1</a></li>
+		<li><a href="">Item 2</a></li>
+		<li><a href="">Item 3</a></li>
+		<li><a href="">Item 4</a></li>
+		<li><a href="">Item 5</a></li>
+	</ul>
+</div>
+</body>
+</html>
